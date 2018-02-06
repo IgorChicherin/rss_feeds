da1808d36bb4273cec3fb3f3a559c70e56ff8729
IgorChicherin <ashzak990@gmail.com>
2018-02-06 17:20:41 +0300

    added commments

--- rss_crud/forms.py
+++ rss_crud/forms.py
@@ -21,13 +21,20 @@ class RssLinkForm(forms.ModelForm):
         model = RssLinks
         fields = ('rss_link',)
 
+
 class RssLinkParseForm(RssLinkForm):
 
     def save(self, commit=True):
+        '''
+        Parse added link and write to database
+        :param commit: boolean
+        :return: class RssLinks
+        '''
         instance = super(RssLinkForm, self).save()
         request = requests.get(instance.rss_link)
         news_feeds = feedparser.parse(request.text)
         for feed in news_feeds['entries']:
             news_item = NewsItems(title=feed['title'], description=feed['description'], rss_link=instance)
             news_item.save()
+        print(type(instance))
         return instance
