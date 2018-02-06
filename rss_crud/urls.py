738184957bf6d18a9b175e8342d41c2bb282a1e8
IgorChicherin <ashzak990@gmail.com>
2018-02-06 16:47:20 +0300

    added create update urls

--- rss_crud/urls.py
+++ rss_crud/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import RssLinksView, RssLinksUpdateView, RssLinksCreateView
+from .views import RssLinksView, RssLinksUpdateView, RssLinksCreateView, RssLinksDeleteView
 
 urlpatterns = [
     url(r'^$', RssLinksView.as_view()),
     url(r'^create/$', RssLinksCreateView.as_view()),
-    url(r'update/(?P<id>\d+)$', RssLinksUpdateView.as_view()),
+    url(r'^update/(?P<pk>\d+)$', RssLinksUpdateView.as_view()),
+    url(r'^delete/(?P<pk>\d+)$', RssLinksDeleteView.as_view()),
 ]
